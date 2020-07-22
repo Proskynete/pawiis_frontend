4199517e4b354fbef36fb2af84a2dd8aecf16604
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-07-22 13:01:25 -0400

    Connect store with app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from './store';
 import Presenter from './views';
 import './assets/scss/main.scss';
 
 import * as serviceWorker from './serviceWorker';
 
 const Main = () => (
-	<BrowserRouter basename='/'>
-		<Presenter />
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter basename='/'>
+			<Presenter />
+		</BrowserRouter>
+	</Provider>
 );
 const root = document.getElementById('root');
 render(<Main />, root);
