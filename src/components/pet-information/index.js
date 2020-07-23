3a5b1be76fe9a1e902562bef22e6d0f78ed1a197
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-07-23 15:41:03 -0400

    Create cards to show information

--- src/components/pet-information/index.js
+++ src/components/pet-information/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
+import './index.scss';
 
 const PetInformation = (props) => {
 	const { information, name } = props;
@@ -7,7 +8,23 @@ const PetInformation = (props) => {
 	return _.isEmpty(information) ? (
 		<p>{_.upperFirst(name)} aún no tiene información.</p>
 	) : (
-		information.length
+		<section className='timeline'>
+			{information.map((info) => (
+				<article key={info._id} className='timeline__item'>
+					<header className='timeline__item__header'>
+						<time
+							dateTime={info.created_at}
+							className='timeline__item__header__create-at'
+						>
+							{info.created_at}
+						</time>
+						<p className='timeline__item__header__creator'>{info.owner_id}</p>
+					</header>
+					<p className='timeline__item__type'>{info.action_type}</p>
+					<p className='timeline__item__description'>{info.description}</p>
+				</article>
+			))}
+		</section>
 	);
 };
 
