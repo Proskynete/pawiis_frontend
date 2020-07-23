c427b345d264f359a01180813568584832d6b788
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-07-23 16:23:24 -0400

    Add new layout to info cards

--- src/components/card-info-pet/index.js
+++ src/components/card-info-pet/index.js
@@ -17,21 +17,23 @@ const CardInfoPet = (props) => {
 
 	return (
 		<article className='timeline__item'>
-			<p className='timeline__item__type'>{prettyType[action_type]}</p>
-			<p className='timeline__item__description'>{description}</p>
-
-			<div className='timeline__item__bottom'>
-				<p className='timeline__item__bottom__creator'>
+			<div className='timeline__item__header'>
+				<p className='timeline__item__header__creator'>
+					<i className='far fa-user' />
 					{handleShowCrreateBy()}
 				</p>
 
 				<time
 					dateTime={onlyDate(created_at)}
-					className='timeline__item__bottom__create-at'
+					className='timeline__item__header__create-at'
 				>
+					<i className='far fa-calendar-alt' />
 					{prettyFormat(created_at)} <small>({fromNow(created_at)})</small>
 				</time>
 			</div>
+
+			<p className='timeline__item__type'>{prettyType[action_type]}</p>
+			<p className='timeline__item__description'>{description}</p>
 		</article>
 	);
 };
